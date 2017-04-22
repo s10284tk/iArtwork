fc93fe7c147718832f1ccf6d4ffdbad028b5aad3
piyo <piyo@macbookpiyo.local>
2017-04-22 23:07:41 +0900

    WebViewのシェアボタンを改良

--- iTunesArtworkiPhone/WebViewController.swift
+++ iTunesArtworkiPhone/WebViewController.swift
@@ -23,21 +23,22 @@ class WebViewController: UIViewController {
   // ボタン押したとき
   @IBAction func buttonPush(_ sender: Any) {
     
-    
     // アラートを作成
-    let alert = UIAlertController(
-      title: "完了",
-      message: "クリップボードに送りました",
-      preferredStyle: .alert)
-
-    alert.addAction(UIAlertAction(title: "OK", style: .default))
-    self.present(alert, animated: true, completion: nil)
+//    let alert = UIAlertController(
+//      title: "完了",
+//      message: "クリップボードに送りました",
+//      preferredStyle: .alert)
+//
+//    alert.addAction(UIAlertAction(title: "OK", style: .default))
+//    self.present(alert, animated: true, completion: nil)
   
     Alamofire.request(itemUrl!).responseImage { response in
       debugPrint(response)
       if let image = response.result.value {
         print("image downloaded: \(image)")
-        self.board.image = image
+        let shareItem = [image]
+        let controller = UIActivityViewController(activityItems: shareItem, applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
       }
     }
   }
