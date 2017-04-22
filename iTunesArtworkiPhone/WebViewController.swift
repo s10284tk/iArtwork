2ce1d036d7c037adc22160d714234304c30150f1
piyo <piyo@macbookpiyo.local>
2017-04-22 16:30:42 +0900

    WebViewControllerがごちゃごちゃしてたのを整理。ただ、もうUIWebView使わないかも。。。

--- iTunesArtworkiPhone/WebViewController.swift
+++ iTunesArtworkiPhone/WebViewController.swift
@@ -43,14 +43,12 @@ class WebViewController: UIViewController {
   }
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-      guard itemUrl != nil else {
+      guard let urlurl = itemUrl else {
         return
       }
-      
-      guard let url = URL(string: itemUrl!) else {
+      guard let url = URL(string: urlurl) else {
         return
       }
       let request = URLRequest(url: url)
@@ -61,18 +59,7 @@ class WebViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
